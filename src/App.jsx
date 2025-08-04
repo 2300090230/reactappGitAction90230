4a836b6d7d57d6ec17dd7af99f559940867d7d20
Laxman <2300090230@kluniversity.in>
2025-08-04 21:55:11 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './App.css'
+
+export default function App() {
+  return (
+    <div className='container'>
+      <h1>Welcome to ReactAppGitAction90230</h1>
+      <p>this is a simple React app</p>
+      <p>it is used to demonstrate GitHub Actions</p>
+      <p>and Continuous Integration</p>
+      <p>with a focus on testing and deployment</p>
+      <p>Enjoy coding!</p>
+      <p>Happy coding!</p>
+      <p>Keep pushing your limits!</p>
+      <p>Let's build something amazing!</p>
+      <p>React is awesome!</p>
+      <p>GitHub Actions makes it better!</p>
+    </div>
+  )
+}
